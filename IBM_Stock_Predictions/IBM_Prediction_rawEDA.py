f9d067f293c3ebed2ebdb29cb38d657ee8bcde41
Hannah <hpeterson6476@gmail.com>
2025-09-27 14:59:35 -0400

    Added to stock prediction set, will update later

---
+++ IBM_Stock_Predictions/IBM_Prediction_rawEDA.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+IBM = pd.read_csv("IBM_Stocks.csv")
+
+# Converting time column to datetime
+IBM['Date'] = pd.to_datetime(IBM['Date'])
+
+
+# My goal with this data set loaded from Kaggle is to display my modeling skills 
+# and abilities. I want to use this set to show how I can take raw data, run
+# tests, create a reliable and statistically significant model, and accurately
+# predict the following days ADJ closing price.
+
+# I will start by creating a predicted "target" column for comparison to my 
+# models predicted price and the actual closing price
+IBM = IBM.sort_values('Date') # assuring organized accurately by date
+IBM['Target'] = IBM['Adj Close'].shift(-1)
+print(IBM.tail())
+print(IBM.shape)
+# I see our last row has a NaN value, which is expected because we have placed 
+# our ADJ close for the following day under the Target column we created, so
+# the last added value does not have the following days ADJ Close because it has
+# not yet happened. To keep our data clean from error, we will remove this row:
+IBM = IBM.dropna()
+print(IBM.tail())
+
+print(IBM.shape) # checking rows after to assure no others were removed by accident
+# and I can confirm only the one row was removed
+
+
+# To begin the model creation process, I am going to select only the variables
+# I see necessary to creating our model:
+predictors = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+x = IBM[predictors]
+y = IBM['Target'] # our prediction column
+
+# It is now important that I run a few tests on the data to accurately select
+# a model for our data type as to avoid high MSE's and general innacuracy's.
+import matplotlib.pyplot as plt # I will model for patterns and outliers
+plt.figure()
+plt.plot(IBM['Date'], IBM['Adj Close'])
+plt.title('IBM Adj Close Over Time')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
